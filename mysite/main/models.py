46a6055c922ba3e79b53740a748c3ca912b9a8ce
Haitham Amireh <haithamamireh@gmail.com>
2021-10-02 13:36:01 +0300

    final-code-but not styles

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#basically u create the DB entities and attributes.
 class WatchList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='watchlist', null=True)
     name = models.CharField(max_length=200)
 
     def __str__(self):
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,23 +6,26 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = WatchList.objects.get(id = id)
-    #{"save":["save"], "c1":["clicked"]}
-    if response.method == 'POST':
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.watchlist.all():
+
+        #{"save":["save"], "c1":["clicked"]}
+        if response.method == 'POST':
+            print(response.POST)
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+                if len(txt) > 2:
+                    ls.item_set.create(text = txt, complete = False)
                 else:
-                    item.complete = False
-                item.save()
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-            if len(txt) > 2:
-                ls.item_set.create(text = txt, complete = False)
-            else:
-                print("invalid input")
-    return render(response,"main/list.html",{"ls":ls})
+                    print("invalid input")
+        return render(response,"main/list.html",{"ls":ls})
+    return render(response,"main/view.html",{})
 
 def home(response):
     return render(response,"main/home.html",{})
@@ -34,7 +37,12 @@ def create(response):
             n = form.cleaned_data["name"]
             w = WatchList(name = n)
             w.save()
+            response.user.watchlist.add(w)
+
         return HttpResponseRedirect("/%i"%w.id)
     else:
         form = CreateNewList()
     return render(response,"main/create.html",{"form":form})
+
+def view(response):
+    return render(response, "main/view.html",{})
